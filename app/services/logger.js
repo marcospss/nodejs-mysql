939fb44e3f4bed139efd283b68af190de00e9809
Marcos Paulo <marcospss@gmail.com>
2017-11-20 19:45:30 -0200

    Configuração da aplicação

---
+++ app/services/logger.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const winston = require('winston');
+const fs = require('fs');
+
+if (!fs.existsSync('logs')) {
+    fs.mkdirSync('logs');
+}
+
+module.exports = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: "info",
+            filename: "logs/api.log",
+            maxsize: 1000,
+            maxFiles: 5
+        })
+    ]
+});
---
+++ config/express.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const morgan = require('morgan');
+const logger = require('./../app/services/logger.js');
+
+const app = express();
+
+app.set('ENV', (process.env.ENV || 'test'));
+app.set('port', (process.env.PORT || 8080));
+app.use(morgan("common", {
+    stream: {
+        write: function(mensagem) {
+            logger.info(mensagem);
+        }
+    }
+}));
+
+app.set('keySecret', 'n0d3J$');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+app.use(expressValidator());
+
+consign({ cwd: 'app' })
+    .include('api')
+    .then('routes')
+    .then('routes/auth.js')
+    .then('services')
+    .into(app);
+
+module.exports = app;
